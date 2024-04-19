b8e626527707c1e9d4f61348c8200ffec2306de1
Louiza Mak <louiza.mak@gmail.com>
2024-04-19 14:52:31 -0500

    complete lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,20 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for dwarf in dwarves:
+        print(f'{dwarves.index(dwarf) + 1}. {dwarf}')
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteers):
+    list = [planeteer.capitalize() + "!" for planeteer in planeteers]
+    return list
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(words):
+    for word in words:
+        if(len(word) > 4):
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(foods):
+    cheese_check = [food for food in foods if food == "cheddar" or food == "gouda" or food == "camembert"]
+    if(len(cheese_check) == 1):
+        return cheese_check[0]
+    else:
+        return None
